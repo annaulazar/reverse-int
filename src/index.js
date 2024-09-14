1843f28c07471ce8cec39cce8c49cb4ac85bd373
Anna Lazarenko <aulazar@yandex.ru>
2024-09-15 01:21:56 +0700

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  nums = String(n).split('');
+  nums = nums.reverse();
+
+  while (nums[0] == 0) {
+      nums = nums.slice(1);
+  }
+  if (nums[nums.length - 1] === '-') {
+      nums.pop();
+  }
+  return Number(nums.join(''));
 }
